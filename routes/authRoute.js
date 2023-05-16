7fbe27d47d666ccf02fba84137123339ce818775
abhisheknayak09 <abhisheknayak0009@gmail.com>
2023-05-17 03:29:11 +0530

    Added Register api and middleware

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import { registerController } from '../controllers/authController.js'
-
+import { registerController , loginController, testController} from '../controllers/authController.js'
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js'
 // router object
 const router = express.Router()
 
@@ -8,5 +8,11 @@ const router = express.Router()
 // Register || Method POST
 
 router.post('/register', registerController)
+
+//Login || POST
+router.post('/login', loginController)
+
+//testroutes
+router.get('/test', requireSignIn, isAdmin, testController)
 
 export default router
