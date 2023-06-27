1484c21524420b4e2a6a2c0f000f19b785280bcc
abhisheknayak09 <abhisheknayak0009@gmail.com>
2023-06-26 23:06:07 -0400

    client

---
+++ client/src/pages/Policy.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Policy = () => {
+  return (
+    <Layout>
+      <div className='row privacy-policy'>
+        <div className='col-md-6'>
+          <img src="/images/policy.svg" alt="aboutus" style={{ width: "100%"}} />
+        </div>
+        <div className='col-md-4'>
+          {/* <h1 className='bg-dark p-2 text-white text-center'>ABOUT US</h1> */}
+          <p className='text-justify mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Pharetra et ultrices neque ornare aenean euismod elementum. A erat nam at lectus urna. Massa id neque aliquam vestibulum morbi blandit cursus. Egestas fringilla phasellus faucibus scelerisque. Adipiscing commodo elit at imperdiet dui accumsan. Duis convallis convallis tellus id interdum velit laoreet. Neque laoreet suspendisse interdum consectetur libero id. Arcu cursus vitae congue mauris. Turpis massa sed elementum tempus. Turpis nunc eget lorem dolor sed viverra ipsum. Magna eget est lorem ipsum. Integer eget aliquet nibh praesent tristique. Molestie at elementum eu facilisis sed. Sagittis vitae et leo duis. </p>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Policy
